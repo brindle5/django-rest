708c7cdfdba0d85e6c913703829e91d4e011a229
brindle5 <sarah.brindle@hotmail.co.uk>
2023-08-09 10:58:10 +0000

    Add like and follower id to serializers

--- followers/models.py
+++ followers/models.py
@@ -3,16 +3,20 @@ from django.contrib.auth.models import User
 
 
 class Follower(models.Model):
+    """
+    Follower model, related to 'owner' and 'followed'.
+    'owner' is a User that is following a User.
+    'followed' is a User that is followed by 'owner'.
+    We need the related_name attribute so that django can differentiate.
+    between 'owner' and 'followed' who both are User model instances.
+    'unique_together' makes sure a user can't 'double follow' the same user.
+    """
     owner = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='following'
-        )
+        User, related_name='following', on_delete=models.CASCADE
+    )
     followed = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='followed'
-        )
+        User, related_name='followed', on_delete=models.CASCADE
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,22 @@
 from rest_framework import generics
+from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
-from drf_api.permissions import IsOwnerOrReadOnly
 
 
-class ProfileList(generics.ListAPIVieww):
-    serializer_class = ProfileSerializer
+class ProfileList(generics.ListAPIView):
+    """
+    List all profiles.
+    No create view as profile creation is handled by django signals.
+    """
     queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve or update a profile if you're the owner.
+    """
     permission_classes = [IsOwnerOrReadOnly]
-    serializer_class = ProfileSerializer
     queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
